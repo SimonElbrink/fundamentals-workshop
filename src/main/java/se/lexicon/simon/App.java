32a6087438bf94193f897ed17d3c2648a8eb28b2
Simon Elbrink <Simon.Elbrink@gmail.com>
2020-02-13 18:22:15 +0100

    ALOT of comments. Also added a method with tests.

--- src/main/java/se/lexicon/simon/App.java
+++ src/main/java/se/lexicon/simon/App.java
@@ -1,21 +1,23 @@
-package se.lexicon.simon;
+package se.lexicon.simon; // Package MOST be at line 1.
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
-        //calling a method inside a method. add() returns the result of the calculation.
+public class App { //Start of Class
+
+    /*
+    Main method, the method that runs our project.
+    Our program lives and dies in the main.
+    if you don't mention it in some form in main it won't happened.
+    */
+    public static void main( String[] args ) //This is a syntax of how you could write a main method.
+    { //Start of main method.
+
+        //Printing out the value of add method. Calling a method inside our sys-out method.
+        //This is just for showing it in action. THIS IS NOT A TEST.
         System.out.println(add(30, 20));
 
-    }
+    } // End of main method
 
     public static int add(int number1, int number2){
-
-        // creating a result, with no value.
+        // creating "result" as int, with no value.
         int result;
 
         //Adding number1 & number2 and saving it as result.
@@ -25,5 +27,18 @@ public class App
         return result;
     }
 
+    public static boolean isAdult(int age){ // Start of isAdult method
 
-}
+        //Creating and assigning ageWhenAdult the value to validate as a adult, saving the value as int.
+        int ageWhenAdult = 18;
+
+        //Validates if age is equal or older then set age restriction, returns the result.
+        if(age >= ageWhenAdult) {
+            return true;
+        } else { //This line specifies the... 1.Closing of "if" statement. 2.Uses the keyword "else" to specify what to do in all other cases when above statements (if-statement) are not valid. 3. Open's "else" scope.
+            return false;
+        } //End of else statement
+    } // End of method
+
+
+} // End of class
--- src/test/java/se/lexicon/simon/AppTest.java
+++ src/test/java/se/lexicon/simon/AppTest.java
@@ -1,8 +1,6 @@
 package se.lexicon.simon;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.*;
 import static se.lexicon.simon.App.*;
 
 import org.junit.Test;
@@ -23,17 +21,54 @@ public class AppTest
 
     @Test
     public void test_add(){
-        //Arrange
+        //Arrange - Preparing all values for my test.
         int number1 = 20;
         int number2 = 30;
         int expected = 50;
         int actual;
 
-        //Act
+        //Act - Executing what i want to test.
         actual = add(number1, number2);
 
-        //Assert
+        //Assert - Checking if my assumption was correct.
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_isAdult_true(){
+        //Arrange
+        int age = 18;
+        boolean expected = true;
+        boolean actual;
+
+        //Act
+        actual = isAdult(age);
+
+        //Assert - Alternative 1
+        assertEquals(expected, actual);
+
+        //Assert - Alternative 2
+        assertTrue(actual);
+
+        //Assert - Alternative 3
+        assertTrue(isAdult(age));
+
+        //These Asserts are all correct, three different ways of achieving the same goal.
+        // Pick the one you understand. Alternative 1 is most explicit.
+    }
+
+    @Test
+    public void test_isAdult_false(){
+
+        //Arrange
+        int age = 10;
+        boolean actual;
+
+        //Act
+        actual = isAdult(age);
+
+        //Assert
+        assertFalse(actual);
 
     }
 
