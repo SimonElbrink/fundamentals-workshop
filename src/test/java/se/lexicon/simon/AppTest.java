c00606684864d913abdf2d61be17aab950aab420
Simon Elbrink <Simon.Elbrink@gmail.com>
2020-02-13 17:03:42 +0100

    Testing add method

--- src/test/java/se/lexicon/simon/AppTest.java
+++ src/test/java/se/lexicon/simon/AppTest.java
@@ -1,6 +1,9 @@
 package se.lexicon.simon;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import static se.lexicon.simon.App.*;
 
 import org.junit.Test;
 
@@ -17,4 +20,23 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void test_add(){
+        //Arrange
+        int number1 = 20;
+        int number2 = 30;
+        int expected = 50;
+        int actual;
+
+        //Act
+        actual = add(number1, number2);
+
+        //Assert
+        assertEquals(expected, actual);
+
+    }
+
+
+
 }
